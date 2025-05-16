bb28be7b65a574ed25fa3f263c79474fdbde33a6
briandenicola <brian@briandenicola.com>
2025-05-16 10:20:38 -0500

    🎳 Fri May 16 10:20:38 CDT 2025

--- argocd/infrastructure/cluster.tf
+++ argocd/infrastructure/cluster.tf
@@ -5,7 +5,7 @@ module "cluster" {
   resource_name        = local.resource_name
   public_key_openssh   = tls_private_key.rsa.public_key_openssh
   tags                 = var.tags
-  kubernetes_version   = "1.28"
+  kubernetes_version   = "1.31"
   sdlc_environment     = "dev"
   vm_sku               = "Standard_D4d_v5"
   node_count           = "3"
--- otel/infrastructure/main.tf
+++ otel/infrastructure/main.tf
@@ -20,7 +20,7 @@ module "cluster" {
   resource_name        = local.resource_name
   public_key_openssh   = tls_private_key.rsa.public_key_openssh
   tags                 = local.tags
-  kubernetes_version   = "1.28"
+  kubernetes_version   = "1.31"
   sdlc_environment     = "dev"
   vm_sku               = "Standard_D4d_v5"
   node_count           = "3"
